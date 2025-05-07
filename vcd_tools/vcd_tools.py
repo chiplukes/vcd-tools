3b6acd654ea7cb89c5f000fb3091cca1939cfffe
Chip Lukes <34627158+chiplukes@users.noreply.github.com>
2025-05-06 22:24:18 -0600

    packaging framework still work in progress

--- vcd_tools/vcd_tools.py
+++ vcd_tools/vcd_tools.py
@@ -1,51 +1,53 @@
-from vcdvcd import VCDVCD
 import questionary
+from vcdvcd import VCDVCD
+
 
 def vcd_tools_fun():
-
-    vcd_path = questionary.path(
-    "Enter path to vcd file."
-    ).ask()
+    vcd_path = questionary.path("Enter path to vcd file.").ask()
 
     # Do the parsing.
     vcd = VCDVCD(vcd_path)
 
     # List all human readable signal names.
-    #print(vcd.references_to_ids.keys())
+    # print(vcd.references_to_ids.keys())
     chc_lst = []
-    for i,key in enumerate(vcd.references_to_ids.keys()):
+    for i, key in enumerate(vcd.references_to_ids.keys()):
         print(key)
-        chc_lst.append(questionary.Choice(f"{key}",value=f"chc{i}",checked=False))
+        chc_lst.append(questionary.Choice(f"{key}", value=f"chc{i}", checked=False))
 
-    options = questionary.checkbox('Select Options:', chc_lst).ask()
+    options = questionary.checkbox("Select Options:", chc_lst).ask()
 
     sigs_lst = []
-    for i,key in enumerate(vcd.references_to_ids.keys()):
+    for i, key in enumerate(vcd.references_to_ids.keys()):
         if chc_lst[i].value in options:
-                print(key)
-                sigs_lst.append(vcd[key])
+            print(key)
+            sigs_lst.append(vcd[key])
 
     num_sigs = len(sigs_lst)
 
     # last time value
     last_time = 0
     for trc in range(num_sigs):
-        last_time = sigs_lst[trc].tv[-1][0] if sigs_lst[trc].tv[-1][0] > last_time else last_time
+        last_time = (
+            sigs_lst[trc].tv[-1][0]
+            if sigs_lst[trc].tv[-1][0] > last_time
+            else last_time
+        )
     beat_cnt = 0
     for i in range(last_time):
-
         # AND all selected signals together
         beat_valid = True
         for trc in range(num_sigs):
             print(sigs_lst[trc][i])
-            if sigs_lst[trc][i] == '0':
+            if sigs_lst[trc][i] == "0":
                 beat_valid = False
 
         if beat_valid:
             beat_cnt += 1
 
-
     print(beat_cnt)
+
+    return True
 
     # # tv is a list of Time/Value delta pairs for this signal.
     # tv = signal.tv
@@ -59,5 +61,3 @@ def vcd_tools_fun():
     # assert(signal[1] == 'x')
     # assert(signal[2] == '0')
     # assert(signal[3] == '0')
-
-
